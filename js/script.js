5169a1ed3ccc441cf8d6f8dcb793a34443b137d4
MarcoGranata9 <granatam9900@gmail.com>
2023-11-06 15:19:43 +0100

    add: milestone 0

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const myTeam = [
+{
+    name : "Wayne Barnett",
+    job : "Founder & CEO",
+    img : "wayne-barnett-founder-ceo.jpg"
+},
+{
+    name : "Angela Caroll",
+    job : "Chief Editor",
+    img : "angela-caroll-chief-editor.jpg"
+},
+{
+    name : "Walter Gordon",
+    job : "Office Manager",
+    img : "walter-gordon-office-manager.jpg"
+},
+{
+    name : "Angela Lopez",
+    job : "Social Media Manager",
+    img : "angela-lopez-social-media-manager.jpg"
+},
+{
+    name : "Scott Estrada",
+    job : "Developer",
+    img : "scott-estrada-developer.jpg"
+},
+{
+    name : "Barbara Ramos",
+    job : "Graphic Designer",
+    img : "barbara-ramos-graphic-designer.jpg"
+}
+]
